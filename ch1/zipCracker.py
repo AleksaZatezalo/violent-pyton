f6dcf8347aefbb8a8a2fbefe4ec47b5cb50d4a6f
aleksa <zabumaphu@gmail.com>
2023-06-18 22:39:19 -0400

    Zip file cracker finished.

--- ch1/zipCracker.py
+++ ch1/zipCracker.py
@@ -5,18 +5,30 @@ Date: June 18th, 2022
 """
 
 import zipfile
+import optparse
 from threading import Thread
 
 def extractFile(zFile, password):
     try:
         zFile.extractall(pwd=password)
-        return password
+        print('[+] Found password ' + password + '\n')
     except:
-        return
+        pass
     
 def main():
-    zFile = zipfile.ZipFile('evil.zip')
-    passFile = open('dictionary.txt')
+    parser = optparse.OptionParser("usage%prog " + "-f <zipFile> -d <dictionary>")
+    parser.add_option('-f', dest='zname', type='string', help='specify zip file')
+    parser.add_option('-d', dest='dname', type='string', help='specify dictionaru file')
+    (options, args) = parser.parse_args()
+    if (options.zname == None) or (options.dname == None):
+        print(parser.usage)
+        exit(0)
+    else:
+        zname = options.zname
+        dname = options.dname
+
+    zFile = zipfile.ZipFile(dname)
+    passFile = open(zname)
     for line in passFile.readlines():
         password = line.strip('\n')
         t = Thread(target=extractFile, args=(zFile, password))
