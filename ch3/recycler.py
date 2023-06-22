9c404f584fb99d1602bb7f4516e481077a450322
aleksa <zabumaphu@gmail.com>
2023-06-21 22:55:10 -0400

    Recovery software found.

---
+++ ch3/recycler.py
@@ -0,0 +1,31 @@
+"""
+Author: Aleksa Zatezalo
+Description: Script created to recover deleted items.
+Date: June 20, 2023
+"""
+
+import os
+
+def returnDir():
+    """
+    Returns the directory of the Recycling bin.
+    """
+    pass
+
+def sid2user():
+    """
+    Correleates SID to user.
+    """
+    pass
+
+def findRecycled():
+    """
+    Find recycled files.
+    """
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
