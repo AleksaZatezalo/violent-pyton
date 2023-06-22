15ff08542eeedacd3918f6cf0ce44e5a2f2b8e20
aleksa <zabumaphu@gmail.com>
2023-06-21 22:50:47 -0400

    WinRegReader finished.

--- ch3/winRegRead.py
+++ ch3/winRegRead.py
@@ -1,8 +1,10 @@
 """
 Author: Aleksa Zatezalo
-Description: File created to read the windows registry.
+Description: File created to read the windows registry and print networks connected too.
 Date: June 20, 2023
 """
+
+from _winreg import *
 
 def val2addr(val):
     """
@@ -20,11 +22,24 @@ def printNets():
     """
     Prints the network information of all the networks the PC has connected to.
     """
-    pass
+    net = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged"
+    key = OpenKey(HKEY_LOCAL_MACHINE, net)
+    print("\n[*] Networks You have Joined.")
+    for i in range(100):
+        try:
+            guid = EnumKey(key, i)
+            netKey = OpenKey(key, str(guid))
+            (n, addr, t) = EnumValue(netKey, 5)
+            (n, name, t) = EnumValue(netKey, 4)
+            macAddr = val2addr(addr)
+            netName = str(name)
+            print("[+] " + netName + " " + macAddr)
+        except:
+            break
 
 
 def main():
-    pass
+    printNets()
 
 if __name__ == "__main__":
-    p
+    main()
