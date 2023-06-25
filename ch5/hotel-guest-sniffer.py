903b308d32aa51e4a3e43cb233a70bd4c38133bb
aleksa <zabumaphu@gmail.com>
2023-06-24 22:33:29 -0400

    Hotel guest sniffer started.

---
+++ ch5/hotel-guest-sniffer.py
@@ -0,0 +1,21 @@
+"""
+Description: A script written in python made to sniff the data of hotel guests.
+Author: Aleksa Zatezalo
+Date: June 23, 2023
+"""
+
+import optparse
+from scapy.all import *
+
+def findGuest(pkt):
+    """
+    Analyizes packet and prints guest information to screen.
+    """
+
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    pass
